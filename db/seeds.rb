07db8cb53bc17c90c726c7d0a211adf34a27efc2
Rafael Barboza <barboza.cardoso@gmail.com>
2013-05-20 17:49:49 -0300

    fixed the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,20 @@
 # coding: utf-8
 
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# The data can then be loaded with the rake db:seed { name: or created alongside the db with db:setup }.
 #
 # Examples:
 #
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
-ActiveRecord::Base.connection.execute "INSERT INTO categories (name) VALUES ('Stockolm'),
- ('Goteborg'),
- ('Malmö'),
- ('Uppsala'),
- ('Orebro'),
- ('Vasteras'),
- ('Linköping'),
- ('Helsinborg'),
- ('Jonkoping'),
- ('Norrköping');"
+#   cities = City.create{ name: [{ :name => 'Chicago' }, { :name => 'Copenhagen' }] }
+#   Mayor.create{ name: :name => 'Daley', :city => cities.first }
+Category.create( name: [
+  { name: 'Stockolm' },
+  { name: 'Goteborg' },
+  { name: 'Malmö' },
+  { name: 'Uppsala' },
+  { name: 'Orebro' },
+  { name: 'Vasteras' },
+  { name: 'Linköping' },
+  { name: 'Helsinborg' },
+  { name: 'Jonkoping' },
+  { name: 'Norrköping' }]);
